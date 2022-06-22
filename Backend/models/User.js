61d0b46e69cbbd8ee78be0a6cbd2e55797be94f7
krishna-creater <krish_n_a@outlook.com>
2022-06-22 22:23:08 +0530

    ready to use express validator

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -10,7 +10,7 @@ const UserSchema = new Schema({
     email:{
         type:String,
         required:true,
-        unique:true
+        // unique:true
     },
     password:{
         type: String,
